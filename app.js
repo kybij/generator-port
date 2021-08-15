72f43fdd0fc246e420309ce88a04a910a5385d5d
Kylee Bijerring <kylee24b@gmail.com>
2021-08-15 16:46:08 -0600

    prompt

--- app.js
+++ app.js
@@ -1,14 +1,24 @@
-const fs = require('fs')
+const inquirer = require('inquirer')
 
-const generatePage = require('./page-template.js')
-const profileDataArgs = process.argv.slice(2)
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?'
+    }
+  ])
+  .then(answers => console.log(answers));
+// const fs = require('fs')
 
-const [name,github] = profileDataArgs
+// const generatePage = require('./page-template.js')
+
+// const [name,github] = profileDataArgs
 
 
 
 
-fs.writeFile('./index.html', generatePage(name,github), err => {
-    if (err) throw new Error(err);
-    console.log('Portfolio complete! Check out index.html to see the output!')
-})
+// fs.writeFile('./index.html', generatePage(name,github), err => {
+//     if (err) throw new Error(err);
+//     console.log('Portfolio complete! Check out index.html to see the output!')
+// })
